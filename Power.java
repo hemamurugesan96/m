1e7666302cdd1671f94f161fa14059b2d6655778
hemamurugesan96 <hemamurugesanece@gmail.com>
2017-06-30 09:53:54 +0530

    Power.java

---
+++ Power.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.util.Scanner;
+class Power
+{
+public static void main(String[] args)
+{
+Scanner sc=new Scanner(System.in);
+int a=sc.nextInt();
+int power=sc.nextInt();
+int r;
+if(a>=1 && power==0)
+{
+r=1;
+}
+else if(a==0 && power>=1)
+{
+r=0;
+}
+else
+{
+for(int i=1;i<=p;i++)
+{
+r=r*a;
+}
+}
+}
+}
